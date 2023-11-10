3690f9be6626fecc84119c023455dfb3eec0e409
JonasSkjodt1 <skjo@itu.dk>
2023-11-10 11:15:50 +0100

    first working peer to peer network created
    Co-authored-by: Alex <lolk@itu.dk>

--- go.mod
+++ go.mod
@@ -7,27 +7,10 @@ require (
 	google.golang.org/protobuf v1.28.1
 )
 
-require (
-	github.com/armon/go-metrics v0.4.1 // indirect
-	github.com/fatih/color v1.14.1 // indirect
-	github.com/hashicorp/errwrap v1.1.0 // indirect
-	github.com/hashicorp/go-cleanhttp v0.5.2 // indirect
-	github.com/hashicorp/go-hclog v1.5.0 // indirect
-	github.com/hashicorp/go-immutable-radix v1.3.1 // indirect
-	github.com/hashicorp/go-multierror v1.1.1 // indirect
-	github.com/hashicorp/go-rootcerts v1.0.2 // indirect
-	github.com/hashicorp/golang-lru v0.5.4 // indirect
-	github.com/hashicorp/serf v0.10.1 // indirect
-	github.com/mattn/go-colorable v0.1.13 // indirect
-	github.com/mattn/go-isatty v0.0.17 // indirect
-	github.com/mitchellh/go-homedir v1.1.0 // indirect
-	github.com/mitchellh/mapstructure v1.5.0 // indirect
-	golang.org/x/exp v0.0.0-20230817173708-d852ddb80c63 // indirect
-)
+require github.com/google/go-cmp v0.5.9 // indirect
 
 require (
 	github.com/golang/protobuf v1.5.2 // indirect
-	github.com/hashicorp/consul/api v1.26.1
 	golang.org/x/net v0.17.0
 	golang.org/x/sys v0.13.0 // indirect
 	golang.org/x/text v0.13.0 // indirect
--- peers/node.go
+++ peers/node.go
@@ -1,3 +1,10 @@
+// how to call the peer to peer network
+// have one terminal connect to
+// go run peers/node.go :5000 localhost:8500
+// have another terminal connect to
+// go run peers/node.go :5001 localhost:8500
+// the peers will then connect
+
 package main
 
 import (
@@ -55,7 +62,7 @@ func main() {
 	}()
 
 	// wait for other nodes to come up
-	time.Sleep(5 * time.Second)
+	time.Sleep(30 * time.Second)
 
 	// setup connection with other node
 	conn, err := grpc.Dial(address, grpc.WithInsecure())
@@ -75,168 +82,3 @@ func main() {
 		time.Sleep(10 * time.Second)
 	}
 }
-
-/*package main
-
-import (
-	"fmt"
-	"log"
-	"net"
-	"os"
-	"strings"
-	"time"
-
-	hs "github.com/Alex-itu/Consensus_gRPC/proto"
-	"github.com/hashicorp/consul/api"
-	"golang.org/x/net/context"
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/reflection"
-)
-
-type node struct {
-	hs.UnimplementedHelloServiceServer
-
-	// Self information
-	Name string
-	Addr string
-
-	// Consul related variables
-	SDAddress string
-	SDKV      api.KV
-
-	// used to make requests
-	Clients map[string]hs.HelloServiceClient
-}
-
-// SayHello implements helloworld.GreeterServer
-func (n *node) SayHello(ctx context.Context, in *hs.HelloRequest) (*hs.HelloReply, error) {
-	return &hs.HelloReply{Message: "Hello from " + n.Name}, nil
-}
-
-// Start listening/service.
-func (n *node) StartListening() {
-
-	lis, err := net.Listen("tcp", n.Addr)
-	if err != nil {
-		log.Fatalf("failed to listen: %v", err)
-	}
-
-	_n := grpc.NewServer() // n is for serving purpose
-
-	hs.RegisterHelloServiceServer(_n, n)
-	// Register reflection service on gRPC server.
-	reflection.Register(_n)
-
-	// start listening
-	if err := _n.Serve(lis); err != nil {
-		log.Fatalf("failed to serve: %v", err)
-	}
-}
-
-// Register self with the service discovery module.
-// This implementation simply uses the key-value store. One major drawback is that when nodes crash. nothing is updated on the key-value store. Services are a better fit and should be used eventually.
-func (n *node) registerService() {
-	config := api.DefaultConfig()
-	config.Address = n.SDAddress
-	consul, err := api.NewClient(config)
-	if err != nil {
-		log.Panicln("Unable to contact Service Discovery.")
-	}
-
-	kv := consul.KV()
-	p := &api.KVPair{Key: n.Name, Value: []byte(n.Addr)}
-	_, err = kv.Put(p, nil)
-	if err != nil {
-		log.Panicln("Unable to register with Service Discovery.")
-	}
-
-	// store the kv for future use
-	n.SDKV = *kv
-
-	log.Println("Successfully registered with Consul.")
-}
-
-// Start the node.
-// This starts listening at the configured address. It also sets up clients for it's peers.
-func (n *node) Start() {
-	// init required variables
-	n.Clients = make(map[string]hs.HelloServiceClient)
-
-	// start service / listening
-	go n.StartListening()
-
-	// register with the service discovery unit
-	n.registerService()
-
-	// start the main loop here
-	// in our case, simply time out for 1 minute and greet all
-
-	// wait for other nodes to come up
-	for {
-		time.Sleep(20 * time.Second)
-		n.GreetAll()
-	}
-}
-
-// Setup a new grpc client for contacting the server at addr.
-func (n *node) SetupClient(name string, addr string) {
-
-	// setup connection with other node
-	conn, err := grpc.Dial(addr, grpc.WithInsecure())
-	if err != nil {
-		log.Fatalf("did not connect: %v", err)
-	}
-	defer conn.Close()
-	n.Clients[name] = hs.NewHelloServiceClient(conn)
-
-	r, err := n.Clients[name].SayHello(context.Background(), &hs.HelloRequest{Name: n.Name})
-	if err != nil {
-		log.Fatalf("could not greet: %v", err)
-	}
-	log.Printf("Greeting from the other node: %s", r.Message)
-
-}
-
-// Busy Work module, greet every new member you find
-func (n *node) GreetAll() {
-	// get all nodes -- inefficient, but this is just an example
-	kvpairs, _, err := n.SDKV.List("Node", nil)
-	if err != nil {
-		log.Panicln(err)
-		return
-	}
-
-	// fmt.Println("Found nodes: ")
-	for _, kventry := range kvpairs {
-		if strings.Compare(kventry.Key, n.Name) == 0 {
-			// ourself
-			continue
-		}
-		if n.Clients[kventry.Key] == nil {
-			fmt.Println("New member: ", kventry.Key)
-			// connection not established previously
-			n.SetupClient(kventry.Key, string(kventry.Value))
-		}
-	}
-}
-
-func main() {
-	// pass the port as an argument and also the port of the other node
-	args := os.Args[1:]
-
-	if len(args) < 3 {
-		fmt.Println("Arguments required: <name> <listening address> <consul address>")
-		os.Exit(1)
-	}
-
-	// args in order
-	name := args[0]
-	listenaddr := args[1]
-	sdaddress := args[2]
-
-	noden := node{Name: name, Addr: listenaddr, SDAddress: sdaddress, Clients: nil} // noden is for opeartional purposes
-
-	// start the node
-	noden.Start()
-}
-*/
